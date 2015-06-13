9428ea76466a4f369744025faa5184f70014960c
Alexf4 <alexf4@gmail.com>
2015-06-13 11:13:03 -0700

    add fake charts to the teachers page

--- routes/teacher.js
+++ routes/teacher.js
@@ -5,6 +5,67 @@
 
 exports.teacherPage = function(req, res ){
 
+  //Total Data stub
+  var totalData = [
+    {
+      value: 300,
+      color: "#a3e1d4",
+      highlight: "#1ab394",
+      label: "Constitutional Underpinnings"
+    },
+    {
+      value: 50,
+      color: "#dedede",
+      highlight: "#1ab394",
+      label: "Political Beliefs and Behaviors"
+    },
+    {
+      value: 100,
+      color: "#b5b8cf",
+      highlight: "#1ab394",
+      label: "Linkage Institutions"
+    },
+    {
+      value: 300,
+      color: "#a3e1d4",
+      highlight: "#1ab394",
+      label: "Institutions of National Government"
+    },
+    {
+      value: 50,
+      color: "#dedede",
+      highlight: "#1ab394",
+      label: "Public Policy"
+    },
+    {
+      value: 100,
+      color: "#b5b8cf",
+      highlight: "#1ab394",
+      label: "Civil Liberties"
+    },
+    {
+      value: 300,
+      color: "#a3e1d4",
+      highlight: "#1ab394",
+      label: "Not Done"
+    }
+  ];
+
+  //Total options
+  var totalOptions = {
+    segmentShowStroke: true,
+    segmentStrokeColor: "#fff",
+    segmentStrokeWidth: 2,
+    percentageInnerCutout: 45, // This is 0 for Pie charts
+    animationSteps: 100,
+    animationEasing: "easeOutBounce",
+    animateRotate: true,
+    animateScale: false,
+    responsive: true,
+  };
+
+
+
 //This is the Constitiutional Underpinnings
   var CUData = [
     {
@@ -39,7 +100,7 @@ exports.teacherPage = function(req, res ){
     responsive: true,
   };
 
-  res.render("teacher", {CUData: CUData, CUOptions: CUOptions});
+  res.render("teacher", {totalData:totalData, totalOptions:totalOptions, CUData: CUData, CUOptions: CUOptions});
 
 };
 
