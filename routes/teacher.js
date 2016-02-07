5096fc8a53be9e8ddbdf91c0e0a084cb3861a715
Alexf4 <alexf4@gmail.com>
2016-02-07 11:16:44 -0800

    added logic for showing the new user dashboard
    works for students and teachers. updated a bug on students.

--- routes/teacher.js
+++ routes/teacher.js
@@ -62,6 +62,7 @@ exports.teacherDrillDown = function (req , res){
   }
   else{
     //render standard view
+
     teacher.teacherPage(req,res);
   }
 
@@ -192,63 +193,99 @@ exports.teacherPage = function(req, res ){
   //Get the users logged in id
   userId = req.session.passport.user;
 
-  chartData = null;
-
-  studentsList = null;
-
-  questionList = null;
-
-
-  //https://github.com/caolan/async#waterfall
-
-  async.waterfall([
-    function(callback) {
-      //Get the teachers students scores/masteries
-      teacherFunctions.getStudentsMasterys(userId, function(scores){
-        callback(null, scores)
-      })
-    },
-    function(scores, callback) {
-      //Convert the scores into a format the front end can consume
-      chartData = dataToChartHelper.createStudentMasteryChart(scores);
-      callback(null)
-    },
-    function(callback){
-
-      //find the teachers class token
-      teacherFunctions.getTeacherClassToken(userId, function(err, classToken) {
-        this.classToken = classToken;
-        callback(null, classToken)
-      })
-    },
-    function(classToken, callback) {
-
-      //create the list of the students in the class
-      teacherFunctions.listStudents(classToken, function(students){
-        studentsList =  students;
-        callback(null)
-      })
-    },
-    function(callback) {
-
-      //create the list of missed questions
-      teacherFunctions.getMissedQuestionsList(userId, function(err, questions){
-        questionList = questions;
-        callback(null);
-      })
+  DBFunctions.isNewUser(userId, function(err, userStatus){
+    if(userStatus){
+      //render new user page
+      teacher.renderNewTeacher(req, res);
     }
-  ],  function () {
-    //Send all the data to the front end.
-    res.render("teacher", {totalData : chartData.totalData , totalOptions : chartData.totalOptions ,
-      CUData : chartData.Constitutional_Underpinnings_Data, SectionOptions : chartData.sectionOptions,
-      Civil_Rights_and_Liberties_Data : chartData.Civil_Rights_and_Liberties_Data,
-      Political_Beliefs_and_Behaviors_Data : chartData.Political_Beliefs_and_Behaviors_Data,
-      Linkage_Institutions_Data: chartData.Linkage_Institutions_Data,
-      Institutions_of_National_Government_Data : chartData.Institutions_of_National_Government_Data,
-      Public_Policy_Data : chartData.Public_Policy_Data, students : studentsList, questions : questionList,
-      Title: "Teacher Dashboard", ClassCode: this.classToken
-    });
+    else {
+      //render teacher page
+      teacher.renderTeacherDashboard(req, res);
+    }
   });
+
+  /**
+   * This method is used to show a new teacher the help diagram
+   * @param req
+   * @param res
+   */
+  exports.renderNewTeacher = function(req, res){
+
+    //TODO: Cody to create a new jade view that holds the jpg. The side nave header and footer should be the same
+    //res.render();
+
+  };
+
+
+  /**
+   * This method is used to show a teacher their dashboard.
+   * @param req
+   * @param res
+   */
+  exports.renderTeacherDashboard = function(req, res){
+    //Get the users logged in id
+    userId = req.session.passport.user;
+
+    chartData = null;
+
+    studentsList = null;
+
+    questionList = null;
+
+
+    //https://github.com/caolan/async#waterfall
+
+    async.waterfall([
+      function(callback) {
+        //Get the teachers students scores/masteries
+        teacherFunctions.getStudentsMasterys(userId, function(scores){
+          callback(null, scores)
+        })
+      },
+      function(scores, callback) {
+        //Convert the scores into a format the front end can consume
+        chartData = dataToChartHelper.createStudentMasteryChart(scores);
+        callback(null)
+      },
+      function(callback){
+
+        //find the teachers class token
+        teacherFunctions.getTeacherClassToken(userId, function(err, classToken) {
+          this.classToken = classToken;
+          callback(null, classToken)
+        })
+      },
+      function(classToken, callback) {
+
+        //create the list of the students in the class
+        teacherFunctions.listStudents(classToken, function(students){
+          studentsList =  students;
+          callback(null)
+        })
+      },
+      function(callback) {
+
+        //create the list of missed questions
+        teacherFunctions.getMissedQuestionsList(userId, function(err, questions){
+          questionList = questions;
+          callback(null);
+        })
+      }
+    ],  function () {
+      //Send all the data to the front end.
+      res.render("teacher", {totalData : chartData.totalData , totalOptions : chartData.totalOptions ,
+        CUData : chartData.Constitutional_Underpinnings_Data, SectionOptions : chartData.sectionOptions,
+        Civil_Rights_and_Liberties_Data : chartData.Civil_Rights_and_Liberties_Data,
+        Political_Beliefs_and_Behaviors_Data : chartData.Political_Beliefs_and_Behaviors_Data,
+        Linkage_Institutions_Data: chartData.Linkage_Institutions_Data,
+        Institutions_of_National_Government_Data : chartData.Institutions_of_National_Government_Data,
+        Public_Policy_Data : chartData.Public_Policy_Data, students : studentsList, questions : questionList,
+        Title: "Teacher Dashboard", ClassCode: this.classToken
+      });
+    });
+  }
+
+
   
 };
 
