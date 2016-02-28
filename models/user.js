e382c9c55e85402c46ddf943ac79f0d47efb809d
Alexf4 <alexf4@gmail.com>
2016-02-28 00:40:03 -0800

    updated imports

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-var questionFunctions = require("../DBWork/questionFunctions");
+var questionFunctions = require("../DBWork/questionFunctions.js");
+
 
 var bcrypt = require('bcrypt-nodejs');
 
